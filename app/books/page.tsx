3cafdb0cb67bed237dfe10f758764437aa31a1dd
YashHB <90118570+waibee-11@users.noreply.github.com>
2024-05-07 18:11:53 -0400

    made books responsive

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -5,10 +5,8 @@ export default function Home(){
         <div className="px-20 py-20 bg-primary-content">
             <div className="text-center text-4xl text-white font-semibold pb-3">Yash&apos;s reading collection</div>
             <div className="text-center text-xl text-white font-light pb-20">As of May 04, 2024 (latest first)</div>
-            <div className="grid grid-cols-3 justify-items-center gap-20">
+            <div className="grid grid-cols-1 xl:grid-cols-3 justify-items-center gap-10">
                 <BookCard title="Murder on the Orient Express" author="Agatha Christie" image="https://lyceumtheatre.org/wp-content/uploads/2019/09/Murder-on-the-Orient-Express-WebPstr.jpg" />
-
-                <BookCard title="The Laws of Power" author="Robert Greene" image="https://www.bestbookstore.ca/cdn/shop/products/9780670881468.jpg?v=1691866257" />
                 <BookCard title="The Subtle Art of Not Giving a F*ck" author="Mark Manson" image="https://m.media-amazon.com/images/I/71QKQ9mwV7L._AC_UF1000,1000_QL80_DpWeblab_.jpg" />
                 <BookCard title="Feel Good Productivity" author="Ali Abdaal" image="https://m.media-amazon.com/images/I/71IaEbGSeuL._AC_UF1000,1000_QL80_.jpg" />
                 <BookCard title="Atomic Habits" author="James Clear" image="https://m.media-amazon.com/images/I/81YkqyaFVEL._AC_UF1000,1000_QL80_.jpg" />
