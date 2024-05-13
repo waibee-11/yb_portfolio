04d800ccebe0bd72b0ad07cd8eaf81e86ea4e1e0
YashHB <90118570+waibee-11@users.noreply.github.com>
2024-05-13 00:55:47 -0400

    weather-page styling (padding)

--- app/weather/page.tsx
+++ app/weather/page.tsx
@@ -13,13 +13,13 @@ export default function WeatherApp(){
             <div className="pb-5">
                 Please note that the link is meant only for development purposes. It is not meant for casual use.
             </div>
-            <div className="font-bold text-red-600 bg-white rounded-full px-3 mb-5">
+            <div className="font-bold text-red-600 bg-white rounded-full px-5 py-2 text-center mb-5">
                 PLEASE DO NOT CLICK ON THE LINK UNLESS YOU GENUINELY WANT TO SEE THE PROJECT.
             </div>
             <div className="">
                 All rights belong to <a href="https://github.com/Rob--W" target="_blank" className="text-primary hover:text-secondary">Rob Wu</a>. You can learn more about this service <a href="https://github.com/Rob--W/cors-anywhere" target="_blank" className="text-primary hover:text-secondary">here</a>.
             </div>
-            <button className="font-bold text-white bg-primary rounded-full px-3 py-1 mb-5 mt-12 hover:bg-secondary transition ease-in-out">
+            <button className="font-bold text-white bg-primary rounded-full px-5 py-2 text-center mb-5 mt-12 hover:bg-secondary transition ease-in-out">
                 <a href="https://main--weather-app-waibee.netlify.app/#">CLICK HERE ONCE YOU&apos;VE GOT TEMPORARY ACCESS TO THE CORS ANYWHERE SERVICE</a>
             </button>
         </div>
