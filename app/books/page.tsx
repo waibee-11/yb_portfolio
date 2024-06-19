a08d0d47c7d5a965f90d47bd17e1ff5de1418cf6
YashHB <90118570+waibee-11@users.noreply.github.com>
2024-06-18 23:13:39 -0400

    added 2 books

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -6,6 +6,8 @@ export default function Home(){
             <div className="text-center text-4xl text-white font-semibold pb-3">Yash&apos;s reading collection</div>
             <div className="text-center text-xl text-white font-light pb-20">As of May 04, 2024 (latest first)</div>
             <div className="grid grid-cols-1 xl:grid-cols-3 justify-items-center gap-10">
+                <BookCard title="The Rudest Book Ever" author="Shwetabh Gangwar" image="https://m.media-amazon.com/images/I/71iUJLYyM8L._AC_UF1000,1000_QL80_.jpg" />
+                <BookCard title="Eat That Frog" author="Brian Tracy" image="https://m.media-amazon.com/images/I/81pDx9j+xQL._AC_UF1000,1000_QL80_.jpg" />
                 <BookCard title="Murder on the Orient Express" author="Agatha Christie" image="https://lyceumtheatre.org/wp-content/uploads/2019/09/Murder-on-the-Orient-Express-WebPstr.jpg" />
                 <BookCard title="The Subtle Art of Not Giving a F*ck" author="Mark Manson" image="https://m.media-amazon.com/images/I/71QKQ9mwV7L._AC_UF1000,1000_QL80_DpWeblab_.jpg" />
                 <BookCard title="Feel Good Productivity" author="Ali Abdaal" image="https://m.media-amazon.com/images/I/71IaEbGSeuL._AC_UF1000,1000_QL80_.jpg" />
